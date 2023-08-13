d969b59d09475b0ba433da75f14acc6da42c9934
holaleche <106927728+withLeche@users.noreply.github.com>
2023-08-13 11:41:47 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,9 @@
+function solution(s1, s2) {
+    let answer = 0;
+    for(let i = 0; i < s1.length; i++) {
+        for(let j = 0; j < s2.length; j++) {
+            s1[i] === s2[j] ? answer++ : answer
+        }
+    }
+    return answer
+}
