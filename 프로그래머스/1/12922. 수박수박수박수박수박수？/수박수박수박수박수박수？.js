9a44aaa851fcb6c4296935cbcb4c7e60f2a09b45
holaleche <106927728+withLeche@users.noreply.github.com>
2023-10-13 11:14:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = []
+    for(let i = 0; i < n; i++){
+        if(i % 2 === 0){
+            answer.push("수");
+        } else {
+            answer.push("박");
+        }
+    }
+    return answer.join('');
+}
