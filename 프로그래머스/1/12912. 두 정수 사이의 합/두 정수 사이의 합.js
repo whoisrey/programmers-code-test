3abb15e07480eba415e7e9c785e799968134b63d
holaleche <106927728+withLeche@users.noreply.github.com>
2023-10-26 09:41:05 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,15 @@
+function solution(a, b) {
+    let sum = 0
+    if(a < b){
+        for(let i = a; i <= b; i++){
+            sum += i
+        }
+    } else if(a > b){
+        for(let i = b; i <= a; i++){
+            sum += i
+        }
+    } else if(a = b){
+        sum = a
+    }
+    return sum
+}
