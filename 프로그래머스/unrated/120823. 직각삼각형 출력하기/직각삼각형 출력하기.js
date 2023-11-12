2e85fb7d2d8d3100643b98181632fd21d9c6af35
holaleche <106927728+withLeche@users.noreply.github.com>
2023-11-12 16:41:28 +0900

    [level unrated] Title: 직각삼각형 출력하기, Time: 42.23 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    let sum = ""
+    for(let i = 0; i < Number(input[0]); i++){
+        console.log(sum += "*")
+    };
+});
