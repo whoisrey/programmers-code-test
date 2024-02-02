1e8b766c3beb28819fcac856cf596cad310cb05b
withleche <106927728+whoisrey@users.noreply.github.com>
2024-02-02 09:22:08 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,13 @@
+function solution(ineq, eq, n, m) {
+    switch(ineq + eq){
+        case ">=":
+            return n >= m ? 1 : 0
+        case "<=":
+            return n <= m ? 1 : 0
+            
+        case ">!":
+            return n > m ? 1 : 0
+        case "<!":
+            return n < m ? 1 : 0
+    }
+}
